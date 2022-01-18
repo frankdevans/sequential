a4b98aaf981e184f19537221f9b608b2a7f69f7e
Frank D. Evans <frankdevans@protonmail.com>
2022-01-17 19:21:46 -0700

    Stable RouterBag FDC, clause ENUM

---
+++ sequential/bags/RouterBag.py
@@ -0,0 +1,48 @@
+from typing import Tuple, List
+from dataclasses import dataclass
+from sly.lex import Token
+
+from ..types import clause
+
+
+
+@dataclass(frozen = True)
+class _ClauseSpan:
+	clause : clause
+	idx_mark : int
+	idx_end : int
+	tokens : Tuple[Token]
+
+
+@dataclass(frozen = True)
+class RouterBag():
+	tokens : Tuple[Token]
+
+	_clauses = {f"C_{name}" : member for (name,member) in clause.__members__.items()}
+
+
+	@property
+	def n_tokens(self)-> int: return len(self.tokens)
+
+	def get_token_clause(self, idx:int) -> clause:
+		token = self.tokens[idx]
+		return self._clauses.get(token.type, clause.NULL)
+
+	@property
+	def clause_spans(self) -> List[_ClauseSpan]:
+		marks = [
+			idx for (idx, token) in enumerate(self.tokens)
+			if (token.type in self._clauses)
+		]
+		ends = [*marks[1:], self.n_tokens]
+		spans = [
+			_ClauseSpan(
+				clause = self.get_token_clause(idx = mark),
+				idx_mark = mark,
+				idx_end = end,
+				tokens = self.tokens[(mark + 1) : end]
+			) for (mark,end) in zip(marks, ends)
+		]
+		return spans
+
+	
---
+++ sequential/types/interface.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class clause(Enum):
+	FROM = 'FROM'
+	SELECT = 'SELECT'
+	WHERE = 'WHERE'
+	HAVING = 'HAVING'
+	LIMIT = 'LIMIT'
+	WITH = 'WITH'
+	UNION = 'UNION'
+	GROUP = 'GROUP'
+	ORDER = 'ORDER'
+	QUERY = 'QUERY'
+	CALC = 'CALC'
+	NULL = '_'
